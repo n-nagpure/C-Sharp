1d44e3cd4912a8c8a10afebc12bea38b3335d9ca
Nikita Nagpure <nikitanagpure2001@gmail.com>
2026-01-05 12:04:52 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿namespace First_project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number;
+            bool isPrime = true;
+
+            Console.WriteLine("Enter a number: ");
+            number = Convert.ToInt32(Console.ReadLine());
+
+            if (number <= 1)
+            {
+                isPrime=false;
+            }
+            for (int j = 2; j < number/2; j++)
+            {
+                if (number % j == 0)
+                {
+                    isPrime = false;
+                }
+            }
+            if (isPrime){
+                Console.WriteLine(number +" is a prime number");
+            }
+            else
+            {
+                Console.WriteLine(number +" is not Prime Number");
+            }
+        }
+    }
+}
