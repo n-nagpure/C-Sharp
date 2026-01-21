756bd285fce1bc259023035ab70334dd61132d9b
Nikita Nagpure <nikitanagpure2001@gmail.com>
2026-01-21 11:43:53 +0530

    Add files via upload

---
+++ Armgstrong.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace Practice
+{
+	class Armgstrong
+	{
+		public static void Main(string[] args)
+		{
+			Console.Write("Enter a number=");
+			int num = int.Parse(Console.ReadLine());
+			int  rem, sum = 0;
+			int num1 = num;
+			while (num1 > 0)
+			{
+				rem = num1 % 10;
+				sum = sum + (rem * rem * rem);
+				num1 /= 10;
+			}
+			if (num == sum)
+			{
+				Console.WriteLine(num + " is Palindrome number");
+			}
+			else
+			{
+				Console.WriteLine(num+" is not Palindrome number");
+			}
+		}
+	}
+}
