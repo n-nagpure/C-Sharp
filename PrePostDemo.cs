06c65253e2a94755ad2ff494357cce6d1b104751
Nikita Nagpure <nikitanagpure2001@gmail.com>
2026-01-21 11:43:15 +0530

    Add files via upload

---
+++ PrePostDemo.cs
@@ -0,0 +1,28 @@
+﻿namespace Practice
+{
+    public class  PrePostDemo
+    {
+        public static void Main()
+        {
+            int x, y;
+            int i;
+            x = 1;
+            Console.WriteLine("Series generated using Postfix y= x + x++");
+            for (i = 0; i < 10; i++)
+            {
+                y = x + x++;
+                Console.WriteLine(y + " ");
+            }
+            Console.WriteLine();
+
+            x=1;
+			Console.WriteLine("Series generated using Prefix y= x + x++");
+			for (i = 0; i < 10; i++)
+			{
+				y = x + ++x;
+				Console.WriteLine(y + " ");
+			}
+			Console.WriteLine();
+		}
+    }
+}
