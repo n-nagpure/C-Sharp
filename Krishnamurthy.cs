cf3684cf3fa8808f2143906a44ef443f22bcb3e5
Nikita Nagpure <nikitanagpure2001@gmail.com>
2026-02-16 22:38:48 +0530

    Add program to check for Krishnamurthy numbers

---
+++ Krishnamurthy.cs
@@ -0,0 +1,25 @@
+using System;
+class Program
+{
+  static void Main()
+    {
+        Console.Write("Enter a number: ");
+        int num = int.Parse(Console.ReadLine());
+        int temp = num;
+        int sum = 0;
+        while (num > 0){
+            int digit = num % 10;
+            int fact = 1;
+            for (int i = 1; i <= digit; i++){
+                fact = fact * i;
+            }
+            sum = sum + fact;
+            num = num / 10;
+        }
+        if (sum == temp)
+            Console.WriteLine("The number is a Krishnamurti Number");
+        else
+            Console.WriteLine("The number is not a Krishnamurti Number");
+              Console.ReadLine();
+    }
+}
