24306c893e856cc22baadb5bd39c78f248cfeebe
Nikita Nagpure <nikitanagpure2001@gmail.com>
2026-03-16 11:47:05 +0530

    Add Boxing class demonstrating boxing in C#

---
+++ Boxing.cpp
@@ -0,0 +1,12 @@
+using System;
+class Boxing
+{
+    static public void Main()
+    {
+        int num = 2020;
+        object obj = num;
+        num = 100;
+        System.Console.WriteLine("Value- type value of num is {0}",num);
+        System.Console.WriteLine("Object- type value of obj is {0}", obj);
+    }
+}
