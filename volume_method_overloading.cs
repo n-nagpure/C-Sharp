2064f7b0a18e3c86f5660e85d425367b13bd3ddc
Nikita Nagpure <nikitanagpure2001@gmail.com>
2026-04-19 22:04:20 +0530

    Implement volume calculation with method overloading

---
+++ volume_method_overloading.cs
@@ -0,0 +1,22 @@
+using System;
+
+class VolumeCalculator{
+    public double Volume(double side){
+        return side*side*side;
+    }
+    public double Volume(double radius, double height){
+        return Math.PI*radius*radius*height;
+    }
+    public double Volume(double length, double width, double height){
+        return length*width*height;
+    }
+}
+
+class Program{
+    public static void Main(){
+        VolumeCalculator calc = new VolumeCalculator();
+        Console.WriteLine($"Cube Volume:{calc.Volume(5)}");
+        Console.WriteLine($"Cylinder Volume:{calc.Volume(3, 4)}");
+        Console.WriteLine($"Square Volume:{calc.Volume(5, 6, 6)}");
+    }
+}
