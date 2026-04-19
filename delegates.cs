394780b9e5dede859b48eb113a877e982872034e
Nikita Nagpure <nikitanagpure2001@gmail.com>
2026-04-19 22:13:16 +0530

    Create delegates.cs

---
+++ delegates.cs
@@ -0,0 +1,43 @@
+Using System; 
+delegate int NumberChanger(int n); 
+namespace DelegateAppl 
+{ 
+class TestDelegate 
+{ 
+static int num = 10; 
+public static int AddNum(int p) 
+{ 
+num += p; 
+return num; 
+} 
+public static int SubNum(int q) 
+{ 
+num -= q; 
+return num; 
+} 
+public static int DivNum(int q) 
+{ 
+num /= q; 
+return num; 
+} 
+public static int getNum() 
+{ 
+return num; 
+} 
+static void Main(string[] args) 
+{ 
+//create delegate instances 
+NumberChanger nc1 = new NumberChanger(AddNum); 
+NumberChanger nc2 = new NumberChanger(SubNum); 
+NumberChanger nc3 = new NumberChanger(DivNum); 
+//calling the methods using the delegate objects 
+nc1(25); 
+Console.WriteLine("Value of Num: {0}", getNum()); 
+nc2(5); 
+Console.WriteLine("Value of Num: {0}", getNum()); 
+nc3(3); 
+Console.WriteLine("Value of Num: {0}", getNum()); 
+Console.ReadKey(); 
+} 
+} 
+} 
