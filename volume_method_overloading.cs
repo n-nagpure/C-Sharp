98881e72edfc9d172e30648b8f2cb46e8e5b240a
Nikita Nagpure <nikitanagpure2001@gmail.com>
2026-04-20 10:17:46 +0530

    Update volume_method_overloading.cs

--- volume_method_overloading.cs
+++ volume_method_overloading.cs
@@ -1,22 +1,28 @@
 using System;
 
-class VolumeCalculator{
-    public double Volume(double side){
-        return side*side*side;
-    }
-    public double Volume(double radius, double height){
-        return Math.PI*radius*radius*height;
-    }
-    public double Volume(double length, double width, double height){
-        return length*width*height;
-    }
+class VolumeCalculator
+{
+	public double Volume(double length, double width, double height)
+	{
+		return length * width * height;
+	}
+	public double Volume(double radius, double height)
+	{
+		return Math.PI * radius * radius * height;
+	}
+	public double Volume(double side)
+	{
+		return side * side * side;
+	}
 }
+class Program
+{
+	static void Main()
+	{
+		VolumeCalculator calc = new VolumeCalculator();
 
-class Program{
-    public static void Main(){
-        VolumeCalculator calc = new VolumeCalculator();
-        Console.WriteLine($"Cube Volume:{calc.Volume(5)}");
-        Console.WriteLine($"Cylinder Volume:{calc.Volume(3, 4)}");
-        Console.WriteLine($"Square Volume:{calc.Volume(5, 6, 6)}");
-    }
+		Console.WriteLine($"Cube Volume: {calc.Volume(5, 6, 6)}");
+		Console.WriteLine($"Cylinder Volume: {calc.Volume(3, 4)}");
+		Console.WriteLine($"Square Volume: {calc.Volume(5)}");
+	}
 }
